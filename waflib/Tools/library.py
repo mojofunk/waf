17ca64d14d00849caf25e16818cc4ed5387ade1e
Tim Mayberry <mojofunk@gmail.com>
2018-02-19 10:11:47 +1000

    Use add_enable_option from option_helpers in library Tool

--- waflib/Tools/library.py
+++ waflib/Tools/library.py
@@ -2,24 +2,13 @@
 # encoding: utf-8
 # Tim Mayberry, 2017
 
+from waflib.Tools.option_helpers import add_enable_option
 
 def options(opt):
-    opt.add_option(
-        '--enable-shared',
-        action='store_true',
-        default=False,
-        help='Build shared libraries')
-    opt.add_option(
-        '--enable-static',
-        action='store_true',
-        default=False,
-        help='Build static libraries')
+    add_enable_option(opt, "shared", True, 'shared libraries')
+    add_enable_option(opt, "static", False, 'static libraries')
 
 
 def configure(conf):
     conf.env.ENABLE_SHARED = conf.options.enable_shared
     conf.env.ENABLE_STATIC = conf.options.enable_static
-
-    if not conf.env.ENABLE_SHARED and not conf.env.ENABLE_STATIC:
-        # needed because of the weird waf options design
-        conf.env.ENABLE_SHARED = True
