d3aadf14a060ea14f825c483364e87cc4b5ed72d
Tim Mayberry <mojofunk@gmail.com>
2018-02-20 11:27:04 +1000

    Add add_with_option to option_helpers Tool/utility functions

--- waflib/Tools/option_helpers.py
+++ waflib/Tools/option_helpers.py
@@ -15,3 +15,15 @@ def add_enable_option(ctx, name, default, feature_str):
         #default = default,
         dest = 'enable_' + name.replace('-','_'),
         help = feature_disable_str )
+
+def add_with_option(ctx, name, default, package_str):
+    package_with_str = 'Build with ' + package_str + ' included'
+    package_without_str = 'Build without ' + package_str + ' included'
+    ctx.add_option('--with-' + name, action = 'store_true',
+        default = default,
+        dest = 'with_' + name.replace('-','_'),
+        help = package_with_str)
+    ctx.add_option('--without-' + name, action = 'store_false',
+        #default = default,
+        dest = 'with_' + name.replace('-','_'),
+        help = package_without_str )
